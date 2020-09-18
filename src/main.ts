5cc87272a290c98c6c22d97309918b878d710b72
Kevin David <kevin.p.david@gmail.com>
2020-09-18 15:57:03 -0400

    Start interesting/uninteresting labels functionality (#1)

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,35 @@ import * as projectSummary from './project-summary';
 
 async function main(): Promise<void> {
     try {
+        const interestingLabels = core.getInput('interestingLabels');
+        let splitInterestingLabels = [];
+        if (interestingLabels)
+        {
+            splitInterestingLabels = interestingLabels.split(",");
+        }
+
         await projectSummary.run({
           projectUrl: core.getInput('project-url'),
           title: core.getInput('title'),
           outputPath: core.getInput('outputPath'),
-          token: core.getInput('token')
+          token: core.getInput('token'),
+          interestingLabels: splitCommaSeperatedArray(core.getInput('interestingLabels')),
+          uninterestingLabels: splitCommaSeperatedArray(core.getInput('uninterestingLabels'))
         });
     } catch (error) {
         core.setFailed(error.message);
     }
+}
+
+function splitCommaSeperatedArray(input: string) : string[]
+{
+    let result: string[] = [];
+    if (input)
+    {
+        result = input.split(",");
+    }
+
+    return result;
 }
 
 main().catch(err => console.error(err));
